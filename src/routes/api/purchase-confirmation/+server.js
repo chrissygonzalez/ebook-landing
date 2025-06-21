f3078167cd696b937942b6a6148c46852e66c4bf
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-06-21 17:41:36 -0400

    Add purchase confirmation

---
+++ src/routes/api/purchase-confirmation/+server.js
@@ -0,0 +1,39 @@
+import { json } from '@sveltejs/kit';
+import sgMail from '@sendgrid/mail';
+import { SENDGRID_API_KEY } from "$env/static/private";
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const PDF_GUIDE_URL = 'https://narrify-public.s3.eu-central-1.amazonaws.com/sample.pdf';
+
+export async function POST({ request }) {
+    const requestBody = await request.json();
+    console.log(requestBody);
+
+    const response = await fetch(PDF_GUIDE_URL);
+    const pdfBuffer = await response.arrayBuffer();
+    const base64Pdf = Buffer.from(pdfBuffer).toString('base64');
+
+    const customerEmail = requestBody.data.object.customer_details.email;
+    const customerName = requestBody.data.object.customer_details.name;
+
+    const message = {
+        to: customerEmail,
+        from: 'chrissygonzalez@gmail.com',
+        subject: 'Your Purchase Confirmation - Complete Spain Relocation Guide',
+        html: `
+        <h1>Thank you for your purchase, ${customerName}</h1>
+        <p>Here's some more text</p>
+        `,
+        attachments: [{
+            constent: base64Pdf,
+            filename: "Digital Ebook",
+            type: "application/pdf",
+            disposition: "attachement",
+        }]
+    }
+
+    await sgMail.send(message);
+
+    return json({ response: 'Email sent' })
+}
